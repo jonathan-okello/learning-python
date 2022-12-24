2d36a7a674da678c1e77efe5d0ac8fb2feeb1b82
jonathan-okello <jonathanokello111@gmail.com>
2022-12-25 00:02:36 +0300

    dv

---
+++ functions.py
@@ -0,0 +1,9 @@
+n = int(input('enter the number\n'))
+list_1 = list(range(n))
+list_2 = list_1.append(5000)
+print(type(list_1))
+"""answer = 1
+def factorial()
+for x in list_1:
+    answer = answer*x
+print(answer)"""    
---
+++ tologin.py
@@ -0,0 +1,4 @@
+from passcode import login
+usernames = ['jona', 'amgura']
+passowords = [1111,2222]
+flag = login(usernames, passowords )
